f85679c23e2d371b791263483a5627f5fb174b9b
Aygun <aygun.gasimova@inbox.ru>
2022-12-02 21:23:23 +0400

    Task21 corrected

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -12,7 +12,7 @@ int EnterXYZ(string str)
 void FindLenght(int x1, int y1, int z1, int x2, int y2, int z2)
 {
     double lenght = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
-    Console.WriteLine(lenght);
+    Console.WriteLine(Math.Round(lenght,2));
 }
 
 
